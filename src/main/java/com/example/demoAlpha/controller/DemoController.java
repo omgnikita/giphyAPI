6ad0c6c39823a3e0810cf4702d60b508b6debffb
omgnikita <omgnikita@yandex.ru>
2022-06-15 17:25:49 +0300

    add view without gif

--- src/main/java/com/example/demoAlpha/controller/DemoController.java
+++ src/main/java/com/example/demoAlpha/controller/DemoController.java
@@ -1,36 +1,44 @@
 package com.example.demoAlpha.controller;
 
 import com.example.demoAlpha.DemoAlphaApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 import static com.example.demoAlpha.DemoAlphaApplication.*;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class DemoController {
 
-    @GetMapping("/name")
-    public String getName() {
+    @GetMapping("/gif")
+    public String getName(Model model) {
         try {
-            if (getRubRateForToday() < getRubRateForYesterday())
-                return (getRichGif());
-            else
-                return(getBrokeGif());
+            if (getRubRateForToday() < getRubRateForYesterday()) {
+                model.addAttribute("URL", getRichGif());
+                System.out.println(getRichGif());
+            } else {
+                model.addAttribute("URL", getBrokeGif());
+                System.out.println(getBrokeGif());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
-         return null;
+        return "gif";
     }
 
     @GetMapping("/address")
-    public String getAddress() {
-        return "Russia!";
+    public String getAddress(Model model) {
+        model.addAttribute("message", "Hello world!");
+        return "gif";
     }
 }
